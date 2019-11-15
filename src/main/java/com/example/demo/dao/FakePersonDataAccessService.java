1bd41c8f2432415eadc28394de2c6c339849cef7
Thanaphoom.b <tp.babparn@gmail.com>
2019-11-15 17:12:55 +0700

    Update full crash course code

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -33,11 +33,23 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public int deletePersonById(UUID id) {
-        return 0;
+        Optional<Person> personMaybe = selectPersonById(id);
+        if(personMaybe.isEmpty()){
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
     }
 
     @Override
-    public int updatePersonById(UUID id, Person person) {
-        return 0;
+    public int updatePersonById(UUID id, Person updatePerson) {
+        return selectPersonById(id).map(person -> {
+            int indexOfPersonToDelete = DB.indexOf(person);
+            if(indexOfPersonToDelete >= 0){
+                DB.set(indexOfPersonToDelete, new Person(id, updatePerson.getName()));
+                return 1;
+            }
+            return 0;
+        }).orElse(0);
     }
 }
